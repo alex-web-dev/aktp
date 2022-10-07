b9919ebc04d792085903ecca6affa0c89a33c684
alex-web-dev <alex.web-dev@mail.ru>
2022-10-07 14:38:04 +0300

    interactive updates

---
+++ src/js/smooth-scroll.js
@@ -0,0 +1,17 @@
+const $anchors = document.querySelectorAll('a[href*="#"]');
+$anchors.forEach($anchor => {
+  $anchor.addEventListener('click', e => {
+    e.preventDefault();
+
+    const id = $anchor.getAttribute('href');
+    if (id === '#') {
+      return;
+    }
+
+    const $elem = document.querySelector(id);
+    if ($elem) {
+      const offsetTop = $elem.getBoundingClientRect().top;
+      window.scrollBy({ top: (offsetTop), left: 0, behavior: 'smooth' });
+    }
+  });
+});
